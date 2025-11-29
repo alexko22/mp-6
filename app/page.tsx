6eed53d31541f17cda3bf76c5b55f908a79a476a
Alex Olson <alexko@bu.edu>
2025-11-28 19:34:10 -0500

    final touches on mp6!

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@
 import SignIn from "@/components/sign-in";
 import Header from "@/components/Header";
 
+// main Home component Function containing header and OAuth components...
+// referred to documentation for the min-h-screen tailwind property to maximize component height to screen
 export default function Home() {
   return (
     <div className="flex w-3/4 m-auto flex-col align-items-center bg-gray-800 min-h-screen">
